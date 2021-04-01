b56855c2ac4ac1edd2a91e1e06a40a3fd10ea744
Muhammad-IqbalMaulana <12050113093@Students.uin-suska.ac.id>
2021-04-01 22:51:54 +0700

    .

---
+++ src/ConditionalsAndLoops/ConditionalStatement.java
@@ -0,0 +1,10 @@
+package ConditionalsAndLoops;
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int x = 5;
+        if ( x == 5){
+            System.out.println("Yeah");
+        }
+    }
+}
