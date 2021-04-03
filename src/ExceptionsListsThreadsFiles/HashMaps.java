6c50a1f9293237acbbb03877226fb6f5ffa775c3
Muhammad-IqbalMaulana <12050113093@Students.uin-suska.ac.id>
2021-04-04 00:24:58 +0700

    .

---
+++ src/ExceptionsListsThreadsFiles/HashMaps.java
@@ -0,0 +1,13 @@
+package ExceptionsListsThreadsFiles;
+
+import java.util.HashMap;
+
+public class HashMaps {
+    public static void main(String[] args) {
+        HashMap<String,String> m = new HashMap<String,String>();
+        m.put("A", "First");
+        m.put("B", "Second");
+        m.put("C", "Third");
+        System.out.println(m.get("B"));
+    }
+}
